e292756d814fee96b3e4ef79f806c9931cf39176
franjojosip <franjojosip.jukic2@gmail.com>
2021-08-24 00:40:42 +0200

    Update index.js

--- src/entry/submitentries/index.js
+++ src/entry/submitentries/index.js
@@ -6,32 +6,7 @@ const serializer = Joi.object({
   entry_ids: Joi.array().required(),
   userId: Joi.string()
 });
-function forEachAsync(array, fun, cb) {
-  var index = 0;
-  if (index == array.length) {
-    cb(null);
-    return;
-  }
 
-  var next = function () {
-    fun(array[index], function (err) {
-      if (err) {
-        cb(err);
-        return;
-      }
-      index++;
-      if (index < array.length) {
-        setImmediate(next);
-        return;
-      }
-
-      //We are done
-      cb(null);
-    });
-  };
-
-  next();
-}
 async function submitEntries(req, res) {
   console.log(req.body);
   try {
@@ -40,28 +15,8 @@ async function submitEntries(req, res) {
       return res.status(400).json({ error: "Poslani su neispravni podatci!" });
     }
     let isError = false;
-    const asyncUppercase = item =>
-      new Promise(resolve =>
-        setTimeout(
-          () => resolve(item.toUpperCase()),
-          Math.floor(Math.random() * 1000)
-        )
-      );
+    const test = await result.value.entry_ids.forEach(async (id) => {
 
-    const uppercaseItems = async () => {
-      const items = ['a', 'b', 'c'];
-      await items.forEach(async item => {
-        const uppercaseItem = await asyncUppercase(item);
-        console.log(uppercaseItem);
-      });
-
-      console.log('Items processed');
-    };
-
-    uppercaseItems();
-
-
-    forEachAsync(result.value.entry_ids, function (e, cb) {
       const submittedEntry = await Entry.findById(id);
       const currentStock = await Stock.findOne({ warehouse_id: submittedEntry.warehouse_id, product_id: submittedEntry.product_id });
 
@@ -77,18 +32,17 @@ async function submitEntries(req, res) {
         await currentStock.save();
       }
       else {
-        console.log("not");
         isError = true;
-      }
-      cb();
-    }, function (err) {
-      console.log("iserror");
-      if (isError) {
-        return res.status(404).json({ error: "Dio proizvoda se ne nalazi na odabranom skladištu, molimo provjerite unose!" });
-      } else {
-        return res.status(200).json({ status: "Uspješno potvrđeni svi unosi!" });
+        console.log("nije");
       }
     });
+    test();
+    console.log("submitted");
+    if (isError) {
+      return res.status(404).json({ error: "Dio proizvoda se ne nalazi na odabranom skladištu, molimo provjerite unose!" });
+    } else {
+      return res.status(200).json({ status: "Uspješno potvrđeni svi unosi!" });
+    }
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Dogodila se pogreška, molimo kontaktirajte administratora!" });
