1971117b7e0b1067db5d6ccab0145ff6637dfa50
franjojosip <franjojosip.jukic2@gmail.com>
2021-08-24 00:51:22 +0200

    Update index.js

--- src/entry/submitentries/index.js
+++ src/entry/submitentries/index.js
@@ -16,7 +16,7 @@ async function submitEntries(req, res) {
       return res.status(400).json({ error: "Poslani su neispravni podatci!" });
     }
     let isError = false;
-    asyncLoop(result.value.entry_ids, (id, next) => {
+    asyncLoop(result.value.entry_ids, function (id, next) {
 
       const submittedEntry = await Entry.findById(id);
       const currentStock = await Stock.findOne({ warehouse_id: submittedEntry.warehouse_id, product_id: submittedEntry.product_id });
@@ -37,7 +37,7 @@ async function submitEntries(req, res) {
         console.log("nije");
       }
       next();
-    }, () => {
+    }, function () {
       console.log("isErrorr");
       if (isError) {
         return res.status(404).json({ error: "Dio proizvoda se ne nalazi na odabranom skladištu, molimo provjerite unose!" });
